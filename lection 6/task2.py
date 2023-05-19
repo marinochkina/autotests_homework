b4a685ba918fe1c8efaecd1207d56f3d0421476d
mn.zagruzina <mn.zagruzina@tensor.ru>
2023-05-19 10:52:40 +0300

    less 6

--- lection 6/task2.py
+++ lection 6/task2.py
@@ -3,13 +3,18 @@
 # Ваша задача дополнить логику функции global_function следующим образом:
 # global_function должна содержать в себе функцию local_function
 # local_function должна изменить значение переменной msg на значение 2
-
 def global_function():
   msg = 1
+
   def local_function():
-  # Здесь нужно написать код
-   
+    nonlocal msg
+    msg = 2
+
+  local_function()
   return msg
+
+
+global_function()
 
 assert global_function() == 2, 'Значение переменной msg должно быть равно 2'
 print('Все ок')
